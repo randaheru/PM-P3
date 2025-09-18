6ca9d826fea970d337e0cc1e8f1f55f28b296859
= <=>
2025-09-18 15:49:20 +0700

    Add history feature

--- bin/kalkulatorbmi.dart
+++ bin/kalkulatorbmi.dart
@@ -1,26 +1,8 @@
-Map<String, dynamic> hitungBMI(double tinggiCm, double beratKg) {
-  // Konversi tinggi cm ke m
-  double tinggiM = tinggiCm / 100;
-
-  // Hitung BMI
-  double bmi = beratKg / (tinggiM * tinggiM);
-
-  // Tentukan kategori
-  String kategori;
-  if (bmi < 18.5) {
-    kategori = "Kurus";
-  } else if (bmi < 25) {
-    kategori = "Normal";
-  } else if (bmi < 30) {
-    kategori = "Gemuk";
-  } else {
-    kategori = "Obesitas";
+void tampilkanRiwayat(List<Map<String, dynamic>> riwayat) {
+  for (var item in riwayat) {
+    print("Tinggi: ${item['tinggi']} cm, "
+          "Berat: ${item['berat']} kg, "
+          "BMI: ${item['bmi'].toStringAsFixed(2)}, "
+          "Kategori: ${item['kategori']}");
   }
-
-  return {
-    'tinggi': tinggiCm,
-    'berat': beratKg,
-    'bmi': bmi,
-    'kategori': kategori
-  };
 }
