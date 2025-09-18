19b5943afc401099ca5f5c7b04bc59165c2990aa
= <=>
2025-09-18 15:46:54 +0700

    Add BMI calculation feature

--- bin/kalkulatorbmi.dart
+++ bin/kalkulatorbmi.dart
@@ -1,5 +1,26 @@
-void validasiInput(double tinggiCm, double beratKg) {
-  if (tinggiCm <= 0 || beratKg <= 0) {
-    throw Exception("Tinggi dan berat harus lebih dari 0");
+Map<String, dynamic> hitungBMI(double tinggiCm, double beratKg) {
+  // Konversi tinggi cm ke m
+  double tinggiM = tinggiCm / 100;
+
+  // Hitung BMI
+  double bmi = beratKg / (tinggiM * tinggiM);
+
+  // Tentukan kategori
+  String kategori;
+  if (bmi < 18.5) {
+    kategori = "Kurus";
+  } else if (bmi < 25) {
+    kategori = "Normal";
+  } else if (bmi < 30) {
+    kategori = "Gemuk";
+  } else {
+    kategori = "Obesitas";
   }
+
+  return {
+    'tinggi': tinggiCm,
+    'berat': beratKg,
+    'bmi': bmi,
+    'kategori': kategori
+  };
 }
